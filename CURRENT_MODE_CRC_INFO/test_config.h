1c94bc2ea31db35c2cf6b4fb4d19c21c4bed4c31
Lucas Vu <lucas.vu@qorvo.com>
2020-04-01 14:33:46 +0700

    Add project

---
+++ CURRENT_MODE_CRC_INFO/test_config.h
@@ -0,0 +1,24 @@
+#ifndef __TEST_CONFIG_H
+#define __TEST_CONFIG_H
+
+#define MODE_SPI_BYPASS		0
+#define MODE_NORMAL				1
+#define MODE_SLEEP				2
+#define MODE_CRC					3
+
+typedef struct
+{
+	volatile unsigned char nOftrim;
+	volatile unsigned char mode;
+	volatile unsigned short unlock;
+} TestConfig;
+
+typedef struct
+{
+	volatile unsigned char value;
+	volatile unsigned char address;
+	volatile unsigned char rfu0;
+	volatile unsigned char rfu1;
+} TrimParams;
+
+#endif	// __TEST_CONFIG_H
